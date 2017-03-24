0cb2ea6fcdaed7cb4b6189fd485546bc0b7c5a1d
Magnun Furtado <magnun.furtado@corp.globo.com>
2017-03-24 14:44:37 -0300

    chore: Initial folder structure

---
+++ buscasrc/search_engine.py
@@ -0,0 +1,6 @@
+class SearchEngine:
+    def __init__(self, value):
+        self.value = "teste"
+
+    def value(self):
+        return self.value
---
+++ tests/buscasrc/test_search_engine.py
@@ -0,0 +1,9 @@
+import unittest
+
+from buscasrc.search_engine import SearchEngine
+
+class TestSearchEgine(unittest.TestCase):
+    def test_example(self):
+        search_engine = SearchEngine("teste")
+
+        self.assertEquals(search_engine.value(), "teste")
