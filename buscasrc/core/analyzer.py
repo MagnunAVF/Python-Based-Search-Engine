ff95e8e04a9074d2bc52e40ceaa73adc63cdfc3e
Magnun Furtado <magnun.furtado@corp.globo.com>
2017-03-30 11:14:44 -0300

    refactor: Refactor Analyzer to deal only with texts
    - Enter: text ; Output: list of filtred tokens.

--- buscasrc/core/analyzer.py
+++ buscasrc/core/analyzer.py
@@ -8,38 +8,31 @@ from buscasrc.core.tokenizer import Tokenizer
 
 class Analyzer:
     """
-        Entity that is responsable for prepare the document to index or to search
+        Entity that is responsable for transform the input text in tokens
     """
     def __init__(self):
-        self.document = {}
+        self.text = {}
 
-
-    def prepare_document(self, document):
-        doc_tokens = self.prepare_text(document["text"])
-
-        tokens_list = self.generate_structure(doc_tokens, document["id"])
-
-        return tokens_list
 
     def prepare_text(self, text):
-        result_text = self.execute_before_filters(text)
+        result_text = self._execute_before_filters(text)
 
         tokenizer = Tokenizer(result_text)
         result_tokens = tokenizer.generate_tokens()
 
-        result_tokens = self.execute_after_filters(result_tokens)
+        result_tokens = self._execute_after_filters(result_tokens)
 
-        return result_tokens
+        return self._generate_tokens_with_positions(result_tokens)
 
 
-    def execute_before_filters(self, text):
+    def _execute_before_filters(self, text):
         punctuation_filter = PunctuationFilter(text)
         result_text = punctuation_filter.filter_text()
 
         return result_text
 
 
-    def execute_after_filters(self, tokens_list):
+    def _execute_after_filters(self, tokens_list):
         lowercase_filter = LowercaseFilter(tokens_list)
         result_tokens = lowercase_filter.filter_tokens()
 
@@ -56,19 +49,19 @@ class Analyzer:
         return result_tokens
 
 
-    def generate_structure(self, tokens_list, document_id):
+    def _generate_tokens_with_positions(self, tokens_list):
         result_tokens_list = []
         tokens_added = []
 
         for token in tokens_list:
             if token not in tokens_added:
                 tokens_added.append(token)
-                result_tokens_list.append( (token, document_id, self.get_token_positions(token, tokens_list)) )
+                result_tokens_list.append( (token, self._get_token_positions(token, tokens_list)) )
 
         return result_tokens_list
 
 
-    def get_token_positions(self, token, token_list):
+    def _get_token_positions(self, token, token_list):
         positions = []
         for id_token, value in enumerate(token_list):
             if value == token:
--- tests/buscasrc/core/test_analyzer.py
+++ tests/buscasrc/core/test_analyzer.py
@@ -6,33 +6,18 @@ class TestAnalyzer(unittest.TestCase):
     def setUp(self):
         self.analyzer = Analyzer()
 
-    def test_prepare_document(self):
-        document = {
-            "id": "ABC001",
-            "text": "Conan, the barbarian is a great HQ. Conan, #MustRead!"
-        }
-
-        self.assertListEqual(self.analyzer.prepare_document(document),
-        [
-            ("conan","ABC001",[0,4]),
-            ("barbarian", "ABC001", [1]),
-            ("great", "ABC001", [2]),
-            ("hq", "ABC001", [3]),
-            ("mustread", "ABC001", [5])
-        ])
-
 
     def test_prepare_text(self):
         text = "Conan, the barbarian is a great HQ. Conan, #MustRead!"
 
-        self.assertListEqual(self.analyzer.prepare_text(text), ["conan", "barbarian", "great", "hq", "conan", "mustread"])
+        self.assertListEqual(self.analyzer.prepare_text(text), [("conan", [0, 4]), ("barbarian", [1]), ("great", [2]), ("hq", [3]), ("mustread", [5])])
 
 
     def test_execute_before_filters(self):
 
         text = "Hello! Can i help you? Some things we have: rice, beans, chicken, ..."
 
-        result = self.analyzer.execute_before_filters(text)
+        result = self.analyzer._execute_before_filters(text)
 
         self.assertEquals(result, "Hello Can i help you Some things we have rice beans chicken ")
 
@@ -40,27 +25,21 @@ class TestAnalyzer(unittest.TestCase):
     def test_execute_after_filters(self):
         tokens_list = ["After", "all", "we", "will", "resist", "-", "JOHN"]
 
-        result = self.analyzer.execute_after_filters(tokens_list)
+        result = self.analyzer._execute_after_filters(tokens_list)
 
         self.assertEquals(result, ["will", "resist", "john"])
 
 
-    def test_generate_structure(self):
+    def test_generate_tokens_with_positions(self):
         tokens_list = ["john", "will", "resist", "john"]
-        document_id = "ABC001"
 
-        result = self.analyzer.generate_structure(tokens_list, document_id)
+        result = self.analyzer._generate_tokens_with_positions(tokens_list)
 
-        self.assertEquals(result,
-        [
-            ("john", "ABC001", [0,3]),
-            ("will", "ABC001", [1]),
-            ("resist", "ABC001", [2])
-        ])
+        self.assertEquals(result, [("john", [0,3]), ("will", [1]), ("resist",[2])])
 
 
     def test_get_token_positions(self):
         token = "conan"
         tokens_list= ["conan", "barbarian", "axe", "conan", "sword"]
 
-        self.assertEquals(self.analyzer.get_token_positions(token, tokens_list), [0,3])
+        self.assertEquals(self.analyzer._get_token_positions(token, tokens_list), [0,3])
