a06e0b107540967832ce3ebdc065152896f1be4a
Magnun Furtado <magnun.furtado@corp.globo.com>
2017-03-30 12:29:05 -0300

    refactor: Change Database to adapts to Index

--- buscasrc/core/database.py
+++ buscasrc/core/database.py
@@ -1,7 +1,36 @@
+# coding: utf-8
+
+import os
+
+
 class Database:
     """
         Entity that is responsable for store the app data
     """
     def __init__(self):
-        self.documents = []
+        self.documents = {}
         self.inverted_index = {}
+
+
+    def index(self, terms, document):
+        self._store_document(document)
+        self._index_terms(document['id'], terms)
+
+
+    def _store_document(self, document):
+        self.documents[document['id']] = document
+
+
+    def _index_terms(self, tokens_list, document):
+        for token in tokens_list:
+            key_name = token[0]
+            position_array = token[1]
+
+            doc_id = document['id']
+
+            # search if exists:
+            if key_name in self.inverted_index.keys():
+                self.inverted_index[key_name][doc_id] = position_array
+            #create if not exists:
+            else:
+                self.inverted_index[key_name] = { doc_id : position_array }
--- tests/buscasrc/core/test_database.py
+++ tests/buscasrc/core/test_database.py
@@ -3,6 +3,32 @@ import unittest
 from buscasrc.core.database import Database
 
 
-class TestDatabase:
-    def test_example(self):
-        raise NotImplementedError
+class TestDatabase(unittest.TestCase):
+    def setUp(self):
+        self.database = Database()
+        self.documentFixture = {
+            "id": "doc001",
+            "text": "Hello! Conan! Have you read Conan HQ? "
+        }
+
+    def test_index(self):
+        tokens_list = [('hello', [0]), ('conan', [1,3]), ('read', [2]), ('hq', [4])]
+
+        self.database._store_document(self.documentFixture)
+        self.database._index_terms(tokens_list, self.documentFixture)
+
+        self.assertEquals(self.database.documents["doc001"], self.documentFixture)
+        self.assertEquals(self.database.inverted_index, {'hello': {'doc001': [0]}, 'conan': {'doc001': [1,3]}, 'read': {'doc001': [2]}, 'hq': {'doc001': [4]}})
+
+    def test_store_document(self):
+        self.database._store_document(self.documentFixture)
+
+        self.assertEquals(self.database.documents["doc001"], self.documentFixture)
+
+
+    def test_index_terms(self):
+        tokens_list = [('hello', [0]), ('conan', [1,3]), ('read', [2]), ('hq', [4])]
+
+        self.database._index_terms(tokens_list, self.documentFixture)
+
+        self.assertEquals(self.database.inverted_index, {'hello': {'doc001': [0]}, 'conan': {'doc001': [1,3]}, 'read': {'doc001': [2]}, 'hq': {'doc001': [4]}})
