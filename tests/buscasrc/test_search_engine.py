f64f4a4468734ab43934010ecb25f481b27deaac
Magnun Furtado <magnun.furtado@corp.globo.com>
2017-07-21 15:11:17 -0300

    test: Improve Search Engine class tests

--- tests/buscasrc/test_search_engine.py
+++ tests/buscasrc/test_search_engine.py
@@ -8,19 +8,18 @@ class TestSearchEgine(unittest.TestCase):
         self.search_engine = SearchEngine()
 
     def test_index_and_search_document(self):
-        doc1 = {'id': 'doc1', 'text': 'Hello obama!'}
-        self.search_engine.index(doc1)
-        doc2 = {'id': 'doc2', 'text': 'Hello president Obama!'}
-        self.search_engine.index(doc2)
-        doc3 = {'id': 'doc3', 'text': 'President Trump will fall.'}
-        self.search_engine.index(doc3)
-        doc4 = {'id': 'doc4', 'text': 'Nanoc the barbarian'}
-        self.search_engine.index(doc4)
+        docs = [
+            {'id': 'doc1', 'text': 'Hello obama!'},
+            {'id': 'doc2', 'text': 'Hello president Obama!'},
+            {'id': 'doc3', 'text': 'President Trump will fall.'},
+            {'id': 'doc4', 'text': 'Nanoc the barbarian'}]
+
+        for doc in docs:
+            self.search_engine.index(doc)
 
         result_docs = self.search_engine.search("President Obama")
-        result_docs.sort()
 
-        self.assertEquals(result_docs, [
+        self.assertEquals(sorted(result_docs), [
                 'Hello obama!',
                 'Hello president Obama!',
                 'President Trump will fall.']
